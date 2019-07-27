afa3e014177ee8543ee2044b194ef31db402da5b
viniciusilveira <viniciussilveira6@gmail.com>
2019-07-26 23:20:39 -0300

    Configure authenticate routes and add authentication in get user route

--- lib/banking_api_web/router.ex
+++ lib/banking_api_web/router.ex
@@ -1,8 +1,14 @@
 defmodule BankingApiWeb.Router do
   use BankingApiWeb, :router
 
+  alias BankingApi.Guardian
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :jwt_authenticated do
+    plug Guardian.AuthPipeline
   end
 
   scope "/api", BankingApiWeb do
@@ -10,6 +16,12 @@ defmodule BankingApiWeb.Router do
 
     post "/sign_up", UserController, :create
     post "/sign_in", UserController, :sign_in
+  end
+
+  scope "/api", BankingApiWeb do
+    pipe_through [:api, :jwt_authenticated]
+
+    get "/user", UserController, :show
   end
 
   scope "/api/documentation" do
